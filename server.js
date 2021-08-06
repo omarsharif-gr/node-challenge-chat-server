8b3c3608688ba6336ac900d42ac3847d7041e57d
omarsharifgr <omarsharif.os@yahoo.com>
2021-08-06 01:07:23 +0100

    Updated Project
    Updated using jQuery implementation, and socket io to implement and carry out this project

--- server.js
+++ server.js
@@ -1,23 +1,91 @@
-const express = require("express");
-const cors = require("cors");
+var express = require('express')
+var bodyParser = require('body-parser')
+var app = express()
+var http = require("http").Server(app)
+var io=require("socket.io")(http)
 
-const app = express();
+app.use(express.static(__dirname))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
 
-app.use(cors());
+var messages = [
+    {name: 'Tim', message: 'Hi'},
+    {name: 'Jane', message: 'Hello'}
+]
 
-const welcomeMessage = {
-  id: 0,
-  from: "Bart",
-  text: "Welcome to CYF chat system!",
-};
+app.get('/messages', (req, res) =>{
+    res.send(messages)
+})
 
-//This array is our "data store".
-//We will start with one message in the array.
-//Note: messages will be lost when Glitch restarts our server.
-const messages = [welcomeMessage];
+app.post('/messages', (req, res) =>{
+  messages.push(req.body)
+  io.emit("message", req.body)
+  res.sendStatus(200)
+})
+io.on("connection", (socket) => {
+  console.log("User connected")
+})
 
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + "/index.html");
-});
+var server = http.listen(3000, () => {
+    console.log('server is listening on port', server.address().port)
+})
 
-app.listen(process.env.PORT);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require("express");
+// const cors = require("cors");
+// const bodyParser = require('body-parser');
+// const { check, validation } = require("express-validator");
+
+// const app = express();
+// app.use(cors());
+
+// const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+// const welcomeMessage = {
+//   id: 0,
+//   from: "Bart",
+//   text: "Welcome to CYF chat system!",
+// };
+
+// //This array is our "data store".
+// //We will start with one message in the array.
+// //Note: messages will be lost when Glitch restarts our server.
+// const messages = [welcomeMessage];
+
+// app.get("/", function (request, response) {
+//   response.sendFile(__dirname + "/index.html");
+// });
+
+// app.get("/messages", function (req, res) {
+//   res.render("messages");
+// });
+
+// app.post("/messages", function (request, response) {
+//   res.json(req.body)
+// })
+
+// const listener = app.listen(process.env.PORT || 3000, function () {
+//   console.log("Your app is listening on port " + listener.address().port);
+// });
